6641cd4d4d2975393cc93017bb8bf4ceb5a8b193
owokaeddy <166180627+owokaeddy@users.noreply.github.com>
2024-05-14 20:03:30 -0500

    new changes by eddy

---
+++ Models/Person.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Aug31_Ver1_WebAp.Models
+{
+    public class Person
+    {
+        public Person()
+        {
+
+        }
+
+        [Key]
+        public int id { get; set; }
+
+        public string? firstname { get; set; }
+
+        public string? lastname { get; set; }
+
+        public int? age { get; set; }
+
+        public string? gender { get; set; }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Aug31_Ver1_WebAp.UnitOfWorkPattern;
+using Aug31_Ver1_WebAp.Interfaces;
+using Microsoft.Extensions.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+//Dependency Injection (IoC)
+builder.Services.AddScoped<IPersonRepository, PersonRepository>();
+
+// Connect EF SQL Server
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+/*builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection"), providerOptions => providerOptions.EnableRetryOnFailure()));*/
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
